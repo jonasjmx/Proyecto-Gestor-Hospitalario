79911e56429589c750fbec6aebef23da893df0b0
GamezNoe <Noemigamez076@gmail.com>
2025-04-22 23:00:25 -0500

    API
    Programn

--- Gestor Hospitalario/Program.cs
+++ Gestor Hospitalario/Program.cs
@@ -3,19 +3,30 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configuración de la conexión a la base de datos
 builder.Services.AddDbContext<HospitalContext>(options =>
-   options.UseMySql(builder.Configuration.GetConnectionString("MySqlConnection"),
-       new MySqlServerVersion(new Version(8, 0, 41)))
+    options.UseMySql(builder.Configuration.GetConnectionString("MySqlConnection"),
+        new MySqlServerVersion(new Version(8, 0, 42)))
 );
-    
-// prueba en el programa(comprobando)
+
+// Habilitar CORS para permitir conexiones desde el frontend
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
+
+// Servicios de la API
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Aplicar configuración de CORS antes de manejar solicitudes
+app.UseCors("AllowAll");
 
 if (app.Environment.IsDevelopment())
 {
@@ -24,9 +35,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
